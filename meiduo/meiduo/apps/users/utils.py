203bacff6929048a80d9f63d538a595713890f57
xsp <xsp1995812@163.com>
2018-09-19 15:23:46 +0800

    jwt状态保持和用户登录功能的实现

--- meiduo/meiduo/apps/users/utils.py
+++ meiduo/meiduo/apps/users/utils.py
@@ -1,3 +1,8 @@
+import re
+
+from django.contrib.auth.backends import ModelBackend
+
+from users.models import User
 
 
 def jwt_response_payload_handler(token,user=None,request=None):
@@ -6,3 +11,17 @@ def jwt_response_payload_handler(token,user=None,request=None):
         'username':user.username,
         'user_id':user.id
     }
+
+class MobileNameView(ModelBackend):
+    #重写用户名验证
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            if re.match(r'1[3-9]\d{9}$',username):
+                user=User.objects.get(mobile=username)
+            else:
+                user = User.objects.get(username =username)
+        except:
+            user = None
+        if user is not None and user.check_password(password):
+            return user
+        return user
